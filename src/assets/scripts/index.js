ccda2956db4f3edb1e40474728af0c5811cd37f4
tylercooksrice <darksilverarrow@gmail.com>
2025-03-16 02:32:45 -0700

    added a service worker

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,7 +1,29 @@
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register("/src/assets/scripts/sw.js", {
+          scope: "/src/assets/scripts/",
+        });
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    }
+  };
+  
+  
+  
+  registerServiceWorker();
 
 document.addEventListener('DOMContentLoaded', () => {
     const toggleSwitch = document.getElementsByTagName("toggle");
-    const currentTheme = "light";
+    var currentTheme = "light";
     currentTheme = localStorage.getItem("theme");
 
     toggleSwitch.addEventListener("clicked", function(){
